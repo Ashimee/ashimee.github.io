500cfa9953aa83ba7f64273c3659be6fe3bcf7fd
Surv <135030944+SurvExe1Pc@users.noreply.github.com>
2023-09-09 17:56:15 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const shell = require('electron').shell;
 const path = require('path');
 const child = require('child_process');
+consst appObj = require('electron').app;
 
 contextBridge.exposeInMainWorld('shellAPI', shell);
 contextBridge.exposeInMainWorld('fileSystemAPI', fs);
 contextBridge.exposeInMainWorld('pathAPI', path);
 contextBridge.exposeInMainWorld('childAPI', child);
+contextBridge.exposeInMainWorld('appAPI', appObj);
