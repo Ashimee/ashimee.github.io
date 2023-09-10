373202e77027a565403e694fd8e3fa5ea4eb4bbd
Surv <135030944+SurvExe1Pc@users.noreply.github.com>
2023-09-10 01:52:25 -0400

    bug fix

--- preload.js
+++ preload.js
@@ -5,7 +5,6 @@ const child = require('child_process');
 const appObj = require('electron').app;
 const fsPromise = require('fs/promises');
 const fs = require('fs');
-import { Buffer } from 'node:buffer';
 
 contextBridge.exposeInMainWorld('fakeAPI', {testing: true});
 
@@ -15,4 +14,3 @@ contextBridge.exposeInMainWorld('fileSystemAPI', fs);
 contextBridge.exposeInMainWorld('pathAPI', path);
 contextBridge.exposeInMainWorld('childAPI', child);
 contextBridge.exposeInMainWorld('appAPI', appObj);
-contextBridge.exposeInMainWorld('Buffer', Buffer);
