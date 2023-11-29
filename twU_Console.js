f96d6f388e8831a750b7d11ba1ea328a5c4548e9
Surv <135030944+SurvExe1Pc@users.noreply.github.com>
2023-11-29 12:27:39 -0500

    error fix

--- twU_Console.js
+++ twU_Console.js
@@ -27,7 +27,7 @@
  *       (Testing the "TWunlocked thinks this is a TurboWarp editor" popup, will be removed if negative feedback)
  */
 var ImportTWunlock = async function (deload, vm) {
-  const VERSION = 5.2.1;
+  const VERSION = 5.201;
 
   //Disable userscript.
   var tmp = null;
